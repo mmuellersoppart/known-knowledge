b2bea84b502819dd480b428f4bc08a690d291c40
mmuellersoppart <mmuellersoppart@gmail.com>
2023-02-11 19:16:50 -0600

    create db

---
+++ database/init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS idea (
+    id              uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+    name            VARCHAR(64) NOT NULL UNIQUE ,
+    description     varchar(256)
+);
+
+INSERT INTO idea  (name, description)
+VALUES
+    ('extract path (axum)', 'extract path parameter'),
+    ('addition', null),
+    ('share state (axum)', 'How to share data between routes'),
+    ('middleware (axum)', 'apply a basic middleware')
+    
