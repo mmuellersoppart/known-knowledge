9a8c52485b79eb7413f561bcdecff4e26f54ec52
mmuellersoppart <mmuellersoppart@gmail.com>
2023-03-11 16:49:08 -0600

    generate migration

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -3,6 +3,7 @@ pub use sea_orm_migration::prelude::*;
 pub mod m20230302_071301_create_idea_table;
 mod m20230304_062227_populate_ideas;
 mod m20230311_085935_create_idea_explanation_table;
+mod m20230311_164722_create_exercise_table;
 
 use sea_orm::{EnumIter, Iterable};
 
@@ -15,6 +16,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20230302_071301_create_idea_table::Migration),
             Box::new(m20230304_062227_populate_ideas::Migration),
             Box::new(m20230311_085935_create_idea_explanation_table::Migration),
+            Box::new(m20230311_164722_create_exercise_table::Migration),
         ]
     }
 }
