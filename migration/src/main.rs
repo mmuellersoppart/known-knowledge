88e2105a21c76a05fde882ce4934df69f9ebe475
mmuellersoppart <mmuellersoppart@gmail.com>
2023-03-11 08:58:33 -0600

    remove sql init
    its done with migrations now

--- migration/src/main.rs
+++ migration/src/main.rs
@@ -2,10 +2,5 @@ use sea_orm_migration::prelude::*;
 
 #[async_std::main]
 async fn main() {
-    tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::DEBUG)
-        .with_test_writer()
-        .init();
-
     cli::run_cli(migration::Migrator).await;
 }
