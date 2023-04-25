9a69faab0c13716aa1406a3d081a5e2e5e16f126
mmuellersoppart <mmuellersoppart@gmail.com>
2023-04-25 09:17:09 -0500

    use sea-query for idea list

--- app/src/routes/ideas_list.rs
+++ app/src/routes/ideas_list.rs
@@ -16,72 +16,55 @@ pub struct ListIdea {
     context: Option<String>,
 }
 
-impl ListIdea {
-    fn from(result: QueryResult) -> ListIdea {
-        ListIdea {
-            id: Default::default(),
-            name: "".to_string(),
-            context: None,
-        }
-    }
-}
+// pub async fn ideas_list(
+//     State(app_state): State<AppState>,
+// ) -> Result<Json<Vec<ListIdea>>, StatusCode> {
+//     let ideas = idea::Entity::find()
+//         .order_by_asc(idea::Column::Name)
+//         .all(&app_state.db)
+//         .await
+//         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+//
+//     let ideas: Vec<ListIdea> = ideas
+//         .iter()
+//         .map(|idea| ListIdea {
+//             id: idea.id,
+//             name: idea.name.clone(),
+//             context: idea.context.clone(),
+//         })
+//         .collect();
+//     Ok(Json(ideas))
+// }
 
 pub async fn ideas_list(
     State(app_state): State<AppState>,
 ) -> Result<Json<Vec<ListIdea>>, StatusCode> {
-    let ideas = idea::Entity::find()
-        .order_by_asc(idea::Column::Name)
-        .all(&app_state.db)
-        .await
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
-    let ideas: Vec<ListIdea> = ideas
-        .iter()
-        .map(|idea| ListIdea {
-            id: idea.id,
-            name: idea.name.clone(),
-            context: idea.context.clone(),
+    let (sql, values) = sea_query::Query::select()
+        .columns([
+            Idea::Id,
+            Idea::Name,
+            Idea::Context,
+        ])
+        .from(Idea::Table)
+        .build(PostgresQueryBuilder);
+
+    let stmt = Statement::from_sql_and_values(DatabaseBackend::Postgres, &sql.as_str(), values);
+
+    let query_result: Vec<QueryResult> = app_state.db.query_all(stmt).await.map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    let ideas: Vec<ListIdea> = query_result.into_iter()
+        .map(|result| {
+            let id = result.try_get::<Uuid>("", "id").expect("failed to get id");
+            let name = result.try_get::<String>("", "name").expect("failed to get name");
+            let context = result.try_get::<Option<String>>("", "context").expect("failed to get context");
+            ListIdea {
+                id,
+                name,
+                context,
+            }
         })
         .collect();
+
     Ok(Json(ideas))
 }
-
-// pub async fn ideas_list(
-//     State(app_state): State<AppState>,
-// ) -> Result<Json<Vec<ListIdea>>, StatusCode> {
-//
-//     let (sql, values) = sea_query::Query::select()
-//         .columns([
-//             Idea::Name,
-//             Idea::Context,
-//             Idea::Description,
-//         ])
-//         .from(Idea::Table)
-//         .build(PostgresQueryBuilder);
-//
-//     let stmt = Statement::from_sql_and_values(DatabaseBackend::Postgres, &sql.as_str(), values);
-//
-//     let query_result: Vec<QueryResult> = app_state.db.query_all(stmt).await.map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-//
-//
-//
-//     Err(StatusCode::INTERNAL_SERVER_ERROR)
-//     // let list_ideas: Vec<ListIdea> = query_result.into_iter().map(|result| ListIdea::from())
-//
-//     //
-//     // let ideas = idea::Entity::find()
-//     //     .order_by_asc(idea::Column::Name)
-//     //     .all(&app_state.db)
-//     //     .await
-//     //     .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-//     //
-//     // let ideas: Vec<ListIdea> = ideas
-//     //     .iter()
-//     //     .map(|idea| ListIdea {
-//     //         id: idea.id,
-//     //         name: idea.name.clone(),
-//     //         context: idea.context.clone(),
-//     //     })
-//     //     .collect();
-//     // Ok(Json(ideas))
-// }
