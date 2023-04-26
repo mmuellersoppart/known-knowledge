b6bd517414225e5a65d4d08e44768aa338266149
mmuellersoppart <mmuellersoppart@gmail.com>
2023-04-26 07:36:09 -0500

    make idea list filterable

--- app/src/routes/ideas_list.rs
+++ app/src/routes/ideas_list.rs
@@ -1,8 +1,11 @@
 use axum::{extract::State, http::StatusCode, Json};
+use axum::extract::Query;
 use sea_orm::{ConnectionTrait, DatabaseBackend, EntityTrait, QueryOrder, QueryResult, Statement, FromQueryResult};
 use sea_query;
-use sea_query::PostgresQueryBuilder;
-use serde::Serialize;
+use sea_query::{Expr, Iden, IntoColumnRef, PostgresQueryBuilder, SelectStatement};
+use sea_query::extension::postgres::PgExpr;
+use serde::{Deserialize, Serialize};
+use tracing::log::{log, Level};
 use uuid::Uuid;
 
 use super::AppState;
@@ -16,38 +19,41 @@ pub struct ListIdea {
     context: Option<String>,
 }
 
-// pub async fn ideas_list(
-//     State(app_state): State<AppState>,
-// ) -> Result<Json<Vec<ListIdea>>, StatusCode> {
-//     let ideas = idea::Entity::find()
-//         .order_by_asc(idea::Column::Name)
-//         .all(&app_state.db)
-//         .await
-//         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-//
-//     let ideas: Vec<ListIdea> = ideas
-//         .iter()
-//         .map(|idea| ListIdea {
-//             id: idea.id,
-//             name: idea.name.clone(),
-//             context: idea.context.clone(),
-//         })
-//         .collect();
-//     Ok(Json(ideas))
-// }
+#[derive(Debug, Deserialize)]
+pub struct Params {
+    name_filter: Option<String>,
+    context_filter: Option<String>
+}
+
+fn apply_filter(mut query: SelectStatement, col: impl IntoColumnRef, text: Option<String>) -> SelectStatement {
+    if let Some(text) = text {
+        return query.and_where(Expr::col(col).ilike(text + "%")).to_owned()
+    }
+
+    query
+}
 
 pub async fn ideas_list(
+    Query(params): Query<Params>,
     State(app_state): State<AppState>,
 ) -> Result<Json<Vec<ListIdea>>, StatusCode> {
 
-    let (sql, values) = sea_query::Query::select()
+
+    log!(Level::Debug, "{params:?}");
+
+     let query = sea_query::Query::select()
         .columns([
             Idea::Id,
             Idea::Name,
             Idea::Context,
         ])
         .from(Idea::Table)
-        .build(PostgresQueryBuilder);
+         .to_owned();
+
+    let query = apply_filter(query, Idea::Name, params.name_filter);
+    let query = apply_filter(query, Idea::Context, params.context_filter);
+
+    let (sql, values) = query.build(PostgresQueryBuilder);
 
     let stmt = Statement::from_sql_and_values(DatabaseBackend::Postgres, &sql.as_str(), values);
 
